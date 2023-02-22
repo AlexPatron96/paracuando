7d15fe6df91e30e26f5a0b7fbb4d4c0e3d967a36
Carlos Bohorquez <113149222+carlosBohorquuez20@users.noreply.github.com>
2023-02-21 21:12:54 -0500

    variables tailwind
    variables tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,49 @@ module.exports = {
   theme: {
 		extend: {
 		  colors: {
-			'my-custom-color': '#fffff'
+      appBlue: '#1B4DB1',
+      appYellow: '#F3F243',
+      appPink: '#FF64BC',
+      appBlack: '#000000',
+      appBlackLight: '#1A1E2E',
+      appGrayDark: '#6E6A6C',           
+      appGray: '#A7A6A7',
+      appGrayLight: '#D9D9D9',
+      appGrayLighter: '#F8F7FA',
+      appRed: '#EF3F47',
 		  },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'title1Size': '48px',
+        'subtitleSize': '16px',
+        'title2Size': '24px',
+        'title3Size': '20px',
+        'text1Size': '15px',
+        'text2Size': '14px',
+      },
+      fontWeight: {
+        'text2Weight': 500,
+        'text1Weight': 400,
+        'subtitle2Weight': 400,
+        'subtitle1Weight': 500,
+        'title2Weight': 500,
+        'title3Weight': 600,
+        'titleWeight': 900,
+      },
+      fontStyle: {
+        'italic': 'italic',
+        'normal': 'normal',
+      },
+      lineHeight: {
+        'title1Height': '48px',
+        'subtitleHeight': '19px',
+        'title2Height': '28px',
+        'title3Height': '23px',
+        'text1Height': '18px',
+        'text2Height': '16px',
+      },
     },
     screens: {
       xs: '375px',

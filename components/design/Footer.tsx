ba2f0843904ab7339b6f7f58a63ecd3b7af0c1ad
Alex Patron <alex.patron@utelvt.edu.ec>
2023-02-23 11:24:26 -0500

    "Configurando Rutas y anadiendo header y footer"

---
+++ components/design/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 p-6 mt-12">
+            <div className="flex justify-center text-white">
+                <p>&copy; MyApp 2023</p>
+                <p>Derechos reservados &copy; 2023</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
